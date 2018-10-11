a7fcb8d68291c3cc29688420628a2afb71ec2530
TheVirginCoder <nfisher2670@gmail.com>
2018-10-11 19:47:20 +0100

    Added resources and Products.xml file

--- StockChecker/FrmStockChecker.Designer.cs
+++ StockChecker/FrmStockChecker.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmStockChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(646, 419);
+            this.Name = "FrmStockChecker";
+            this.Text = "Stock Checker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
