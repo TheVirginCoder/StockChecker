ee238cdd46c768d520fc9ec83595c09b984bc4b3
TheVirginCoder <nfisher2670@gmail.com>
2018-10-21 23:44:02 +0100

    Designed basic form

--- StockChecker/Form1.cs
+++ StockChecker/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StockChecker
 {
-    public partial class Form1 : Form
+    public partial class FrmStockCheckerMain : Form
     {
-        public Form1()
+        public FrmStockCheckerMain()
         {
             InitializeComponent();
         }
