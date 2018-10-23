aa5b3ecfcd12f02c04efa535bc09efcb7c11047d
TheVirginCoder <nfisher2670@gmail.com>
2018-10-23 19:01:27 +0100

    Added code for searching products

--- StockChecker/FrmStockCheckerMain.cs
+++ StockChecker/FrmStockCheckerMain.cs
@@ -18,5 +18,38 @@ namespace StockChecker
         {
             InitializeComponent();
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtProductName.Text))
+                {
+                    MessageBox.Show("Error: Please enter a value for the units in stock");
+                    return;
+                }
+
+                XPathNavigator nav;
+                XPathDocument docNav;
+                XPathNodeIterator nodeIter;
+                string strExpression;
+
+                docNav = new XPathDocument(@"C:\Users\FIS40063783\source\repos\ComboBoxExample\ComboBoxExample\res\Products.xml");
+                nav = docNav.CreateNavigator();
+                strExpression = "/AllCategories/Category/Products/Product/ProductName[../UnitsInStock <=" + txtProductName.Text + "]";
+
+                nodeIter = nav.Select(strExpression);
+                lstProducts.Items.Clear();
+                lstProducts.Items.Add("List of stock that requires reordering: ");
+
+                while (nodeIter.MoveNext())
+                {
+                    lstProducts.Items.Add(nodeIter.Current.Value);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
