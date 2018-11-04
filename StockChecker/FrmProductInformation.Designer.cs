622f6d914b82b0169673a81a03353476c432a44d
TheVirginCoder <nfisher2670@gmail.com>
2018-11-04 16:12:59 +0000

    Redesigned Product Information Form

--- StockChecker/FrmProductInformation.Designer.cs
+++ StockChecker/FrmProductInformation.Designer.cs
@@ -28,81 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblProductID = new System.Windows.Forms.Label();
             this.cbxProductIDList = new System.Windows.Forms.ComboBox();
             this.lblProductName = new System.Windows.Forms.Label();
             this.txtProductName = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnReorderList = new System.Windows.Forms.Button();
+            this.btnReturnToMenu = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblProductID
-            // 
-            this.lblProductID.AutoSize = true;
-            this.lblProductID.Location = new System.Drawing.Point(13, 60);
-            this.lblProductID.Name = "lblProductID";
-            this.lblProductID.Size = new System.Drawing.Size(58, 13);
-            this.lblProductID.TabIndex = 1;
-            this.lblProductID.Text = "Product ID";
-            // 
             // cbxProductIDList
             // 
+            this.cbxProductIDList.BackColor = System.Drawing.Color.Red;
             this.cbxProductIDList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxProductIDList.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxProductIDList.ForeColor = System.Drawing.Color.White;
             this.cbxProductIDList.FormattingEnabled = true;
-            this.cbxProductIDList.Location = new System.Drawing.Point(110, 57);
+            this.cbxProductIDList.Location = new System.Drawing.Point(347, 12);
             this.cbxProductIDList.Name = "cbxProductIDList";
-            this.cbxProductIDList.Size = new System.Drawing.Size(121, 21);
+            this.cbxProductIDList.Size = new System.Drawing.Size(154, 26);
             this.cbxProductIDList.TabIndex = 2;
             // 
             // lblProductName
             // 
             this.lblProductName.AutoSize = true;
-            this.lblProductName.Location = new System.Drawing.Point(13, 95);
+            this.lblProductName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.ForeColor = System.Drawing.Color.Red;
+            this.lblProductName.Location = new System.Drawing.Point(43, 15);
             this.lblProductName.Name = "lblProductName";
-            this.lblProductName.Size = new System.Drawing.Size(75, 13);
+            this.lblProductName.Size = new System.Drawing.Size(130, 18);
             this.lblProductName.TabIndex = 3;
-            this.lblProductName.Text = "Product Name";
+            this.lblProductName.Text = "Search Criteria";
             // 
             // txtProductName
             // 
-            this.txtProductName.Location = new System.Drawing.Point(110, 92);
+            this.txtProductName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductName.ForeColor = System.Drawing.Color.Red;
+            this.txtProductName.Location = new System.Drawing.Point(179, 13);
             this.txtProductName.Name = "txtProductName";
-            this.txtProductName.Size = new System.Drawing.Size(121, 20);
+            this.txtProductName.Size = new System.Drawing.Size(162, 26);
             this.txtProductName.TabIndex = 4;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(110, 129);
+            this.btnSearch.BackColor = System.Drawing.Color.Red;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(179, 39);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.Size = new System.Drawing.Size(101, 51);
             this.btnSearch.TabIndex = 5;
             this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseVisualStyleBackColor = false;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.btnSearch.MouseEnter += new System.EventHandler(this.btnSearch_MouseEnter);
+            this.btnSearch.MouseLeave += new System.EventHandler(this.btnSearch_MouseLeave);
             // 
             // dgvProducts
             // 
             this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProducts.Location = new System.Drawing.Point(237, 37);
+            this.dgvProducts.Location = new System.Drawing.Point(16, 91);
             this.dgvProducts.Name = "dgvProducts";
-            this.dgvProducts.Size = new System.Drawing.Size(396, 295);
+            this.dgvProducts.Size = new System.Drawing.Size(731, 319);
             this.dgvProducts.TabIndex = 6;
             // 
-            // FrmStockCheckerMain
+            // btnReorderList
+            // 
+            this.btnReorderList.BackColor = System.Drawing.Color.Red;
+            this.btnReorderList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReorderList.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReorderList.ForeColor = System.Drawing.Color.White;
+            this.btnReorderList.Location = new System.Drawing.Point(130, 416);
+            this.btnReorderList.Name = "btnReorderList";
+            this.btnReorderList.Size = new System.Drawing.Size(101, 51);
+            this.btnReorderList.TabIndex = 7;
+            this.btnReorderList.Text = "Reorder List";
+            this.btnReorderList.UseVisualStyleBackColor = false;
+            this.btnReorderList.Click += new System.EventHandler(this.btnReorderList_Click);
+            this.btnReorderList.MouseEnter += new System.EventHandler(this.btnReorderList_MouseEnter);
+            this.btnReorderList.MouseLeave += new System.EventHandler(this.btnReorderList_MouseLeave);
+            // 
+            // btnReturnToMenu
+            // 
+            this.btnReturnToMenu.BackColor = System.Drawing.Color.Red;
+            this.btnReturnToMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReturnToMenu.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturnToMenu.ForeColor = System.Drawing.Color.White;
+            this.btnReturnToMenu.Location = new System.Drawing.Point(12, 416);
+            this.btnReturnToMenu.Name = "btnReturnToMenu";
+            this.btnReturnToMenu.Size = new System.Drawing.Size(101, 51);
+            this.btnReturnToMenu.TabIndex = 8;
+            this.btnReturnToMenu.Text = "Return To Menu";
+            this.btnReturnToMenu.UseVisualStyleBackColor = false;
+            this.btnReturnToMenu.Click += new System.EventHandler(this.btnReturnToMenu_Click);
+            this.btnReturnToMenu.MouseEnter += new System.EventHandler(this.btnReturnToMenu_MouseEnter);
+            this.btnReturnToMenu.MouseLeave += new System.EventHandler(this.btnReturnToMenu_MouseLeave);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(646, 416);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(101, 51);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseEnter += new System.EventHandler(this.btnExit_MouseEnter);
+            this.btnExit.MouseLeave += new System.EventHandler(this.btnExit_MouseLeave);
+            // 
+            // FrmProductInformation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 371);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(759, 479);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReturnToMenu);
+            this.Controls.Add(this.btnReorderList);
             this.Controls.Add(this.dgvProducts);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtProductName);
             this.Controls.Add(this.lblProductName);
             this.Controls.Add(this.cbxProductIDList);
-            this.Controls.Add(this.lblProductID);
-            this.Name = "FrmStockCheckerMain";
+            this.Name = "FrmProductInformation";
             this.Text = "Product Information";
-            this.Load += new System.EventHandler(this.FrmStockCheckerMain_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmProductInformation_FormClosed);
+            this.Load += new System.EventHandler(this.FrmProductInformation_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -110,12 +168,14 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label lblProductID;
         private System.Windows.Forms.ComboBox cbxProductIDList;
         private System.Windows.Forms.Label lblProductName;
         private System.Windows.Forms.TextBox txtProductName;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btnReorderList;
+        private System.Windows.Forms.Button btnReturnToMenu;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- StockChecker/FrmProductInformation.cs
+++ StockChecker/FrmProductInformation.cs
@@ -48,7 +48,7 @@ namespace StockChecker
             }
         }
 
-        private void FrmStockCheckerMain_Load(object sender, EventArgs e)
+        private void FrmProductInformation_Load(object sender, EventArgs e)
         {
             try
             {
@@ -70,6 +70,77 @@ namespace StockChecker
             {
                 MessageBox.Show("Error: " + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void btnReturnToMenu_Click(object sender, EventArgs e)
+        {
+            FrmMainMenu returnToMain = new FrmMainMenu();
+            returnToMain.Show();
+            this.Dispose();
+            this.Close();
+        }
+
+        private void btnReorderList_Click(object sender, EventArgs e)
+        {
+            FrmReorderList reorderList = new FrmReorderList();
+            reorderList.Show();
+            this.Dispose();
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult exit = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (exit.Equals(DialogResult.Yes))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnReturnToMenu_MouseEnter(object sender, EventArgs e)
+        {
+            btnReturnToMenu.BackColor = Color.Navy;
+        }
+
+        private void btnReturnToMenu_MouseLeave(object sender, EventArgs e)
+        {
+            btnReturnToMenu.BackColor = Color.Red;
+        }
+
+        private void btnReorderList_MouseEnter(object sender, EventArgs e)
+        {
+            btnReorderList.BackColor = Color.Navy;
+        }
+
+        private void btnReorderList_MouseLeave(object sender, EventArgs e)
+        {
+            btnReorderList.BackColor = Color.Red;
+        }
+
+        private void btnExit_MouseEnter(object sender, EventArgs e)
+        {
+            btnExit.BackColor = Color.Navy;
+        }
+
+        private void btnExit_MouseLeave(object sender, EventArgs e)
+        {
+            btnExit.BackColor = Color.Red;
+        }
+
+        private void btnSearch_MouseEnter(object sender, EventArgs e)
+        {
+            btnSearch.BackColor = Color.Navy;
+        }
+
+        private void btnSearch_MouseLeave(object sender, EventArgs e)
+        {
+            btnSearch.BackColor = Color.Red;
+        }
+
+        private void FrmProductInformation_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
